6d0865cb07dfc8b63b9dfeb5fcb4cc3b20f0fd07
Brad Bambara <brad.bambara@eachscape.com>
2012-12-31 19:52:36 -0500

    starting to use libxml2 to parse the schema document

--- NoPLGenerator/main.cpp
+++ NoPLGenerator/main.cpp
@@ -11,6 +11,64 @@
 #include "NoPLc.h"
 #include "NoPLRuntime.h"
 
+#include <libxml/parser.h>
+#include <libxml/xmlschemas.h>
+#include <libxml/schemasInternals.h>
+
+#pragma mark - Schema
+
+xmlSchemaPtr loadSchemaAtPath(const char* schemaPath)
+{
+	//load the schema document from file
+	xmlDocPtr xmlDoc = xmlReadFile(schemaPath, NULL, XML_PARSE_XINCLUDE|XML_PARSE_NONET);//TODO: what are the settings we want here?
+	if(!xmlDoc)
+		return NULL;
+	
+	//attempt to create a parser
+	xmlSchemaParserCtxtPtr parser = xmlSchemaNewDocParserCtxt(xmlDoc);
+	if(!parser)
+	{
+		xmlFreeDoc(xmlDoc);
+		return NULL;
+	}
+	
+	//attempt to parse the document as a schema
+	xmlSchemaPtr schemaDoc = xmlSchemaParse(parser);
+	
+	//free the objects that were used to load
+	xmlSchemaFreeParserCtxt(parser);
+	xmlFreeDoc(xmlDoc);
+
+	return schemaDoc;
+}
+
+void evaluateType(void * payload, void * data, xmlChar * name)
+{
+	printf("Type: %s\n", name);
+}
+
+void evaluateAttribute(void * payload, void * data, xmlChar * name)
+{
+	printf("Attribute: %s\n", name);
+}
+
+void evaluateAttributeGroup(void * payload, void * data, xmlChar * name)
+{
+	printf("Attribute Group: %s\n", name);
+}
+
+void evaluateElement(void * payload, void * data, xmlChar * name)
+{
+	printf("Element: %s\n", name);
+//	xmlNodePtr node = (xmlNodePtr)payload;//TODO: is this a xmlNodePointer?
+	
+}
+
+void evaluateNotation(void * payload, void * data, xmlChar * name)
+{
+	printf("Notation: %s\n", name);
+}
+
 #pragma mark - NoPL callbacks
 
 //TODO: nopl callbacks
@@ -29,7 +87,18 @@ int main(int argc, const char * argv[])
 	const char* schemaFilePath = argv[1];
 	const char* scriptFilePath = argv[2];
 	
-	
+	//load the schema
+	xmlSchemaPtr schema = loadSchemaAtPath(schemaFilePath);
+	if(schema->typeDecl)
+		xmlHashScan(schema->typeDecl, evaluateType, NULL);
+	if(schema->attrDecl)
+		xmlHashScan(schema->attrDecl, evaluateAttribute, NULL);
+	if(schema->attrgrpDecl)
+		xmlHashScan(schema->attrgrpDecl, evaluateAttributeGroup, NULL);
+	if(schema->elemDecl)
+		xmlHashScan(schema->elemDecl, evaluateElement, NULL);
+	if(schema->notaDecl)
+		xmlHashScan(schema->notaDecl, evaluateNotation, NULL);
 	
     return 0;
 }
